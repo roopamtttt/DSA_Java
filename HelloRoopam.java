3c2e4762ae70bd86fc1a1fcd956e0ddec3c2adb0
apple <apple@apples-MacBook-Air.local>
2025-03-10 10:12:03 -0700

    Initial commit

---
+++ HelloRoopam.java
@@ -0,0 +1,5 @@
+public class HelloRoopam {
+    public static void main (String [] args){
+        System.out.print("Sonali, Good Morning!");
+    }
+}
---
+++ HiSonlai.java
@@ -0,0 +1,5 @@
+public class HiSonlai {
+    public static void main (String [] args){
+        System.out.println("Hi Sonlai");
+    }
+}
